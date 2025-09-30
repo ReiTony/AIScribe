e20145ce1f24c132426b68612e508964590e5780
ReiTony <reimerstony@gmail.com>
2025-09-30 09:38:08 +0800

    refactor: remove unused API routers and add chat endpoint implementation

--- utils/hashing.py
+++
@@ -1,9 +0,0 @@
-"""Hash utilities for cache keys & template integrity (skeleton)."""
-import hashlib
-import json
-from typing import Any
-
-
-def stable_hash(obj: Any) -> str:
-    data = json.dumps(obj, sort_keys=True, separators=(",", ":"))
-    return hashlib.sha256(data.encode("utf-8")).hexdigest()
